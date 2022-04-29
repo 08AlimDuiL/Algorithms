54f05829116b29d065fb37e249de0c75b6ea4126
08AlimDuil <lyulyudvik@gmail.com>
2022-04-29 09:04:30 +0300

    Second

--- src/main/java/hw9/OddEven.java
+++ src/main/java/hw9/OddEven.java
@@ -1,4 +1,19 @@
+//----------------------------------Task №1-------------------------------------
+//Создать алгоритм OddEven, который принимает на вход целое число,
+// возвращает “Odd”,  если число нечетное, и “Even”, если число четное.
+// Во всех остальных случаях результат будет “Undefined”.
 package hw9;
 
 public class OddEven {
+    public String OddEven (int a) {
+        String result;
+        if (a % 2 != 0) {
+            result = "Odd";
+        } else if (a % 2 == 0) {
+            result = "Even";
+        } else {
+            result = "Undefined";
+        }
+        return result;
+    }
 }
